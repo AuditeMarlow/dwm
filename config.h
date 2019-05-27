30720fbbbd94cf41c77be9162fce5cfbf73cffd8
Niek den Breeje <n.denbreeje@guardian360.nl>
2019-05-27 14:05:07 +0200

    feat: Add custom application rules

--- config.h
+++ config.h
@@ -28,9 +28,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class         instance    title       tags mask     isfloating   monitor */
+        { "qutebrowser", NULL,       NULL,       1 << 2,       0,           -1 },
+        { "Chromium",    NULL,       NULL,       1 << 6,       0,           -1 },
+        { "Spotify" ,    NULL,       NULL,       1 << 8,       0,           -1 },
+        { "Telegram",    NULL,       NULL,       1 << 9,       0,           -1 },
 };
 
 /* layout(s) */
